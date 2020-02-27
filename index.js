73bfc32b29c2ae147f94f2a334c1ec40791360d7
Henry Nelson <Henry.Nelson89@gmail.com>
2020-02-27 15:04:35 -0800

    all done!

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ class Car {
     if(distance < max){
     this.tank -= distance / this.milesPerGallon;
     this.odometer += distance;
-    } else if (distance >= max){
+    } else {
       this.odometer += max;
       this.tank -= max/this.milesPerGallon
       return `I ran out of fuel at ${this.odometer} miles!`
@@ -208,8 +208,18 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager  extends Instructor{
+  constructor(attributes){
+    super(attributes)
+    this.gradClassName = attributes.gradClassName
+    this.favInstructor = attributes.favInstructor
+  };
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  };
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  };
 }
 
 /*
