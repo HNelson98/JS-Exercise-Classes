d84bc148ad65049d02975ff924b54b5126647e99
Henry Nelson <Henry.Nelson89@gmail.com>
2020-02-27 14:14:22 -0800

    done with 5!

--- index.js
+++ index.js
@@ -159,8 +159,8 @@ demo(subject){
 };
 grade(student, subject){
   return `${student.name} receives a perfect score on ${subject}`
-}
-}
+};
+};
 
 /*
   TASK 5
@@ -177,8 +177,22 @@ grade(student, subject){
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(attributes){
+  super(attributes)
+    this.previousBackground = attributes.previousBackground,
+    this.className = attributes.className,
+    this.favSubjects = attributes.favSubjects
+  };
+  listSubjects(){
+    return `Loving ${this.favSubjects}`
+  };
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+  };
+  sprintChallenge(subject){
+    return `${this.name} has begun a sprint challenge on ${subject}`
+  };
 }
 
 /*
